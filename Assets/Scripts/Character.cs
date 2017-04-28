753a6e3eb1504a765270ebc2f9a1b4171d26f10c
Sava Minić <savam@nordeus.com>
2017-04-28 23:41:09 +0200

    Added spider and fly models

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Character : MonoBehaviour
+{
+	[SerializeField]
+	private Animator animator;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var dx = Input.GetAxis("Horizontal");
+		var dy = Input.GetAxis("Vertical");
+
+		animator.SetFloat("speedX", dx);
+		animator.SetFloat("speedY", dy);
+
+		if (dx != 0f || dy != 0f)
+		{
+			animator.SetBool("walking", true);
+		}
+		else
+		{
+			animator.SetBool("walking", false);
+		}
+	}
+}

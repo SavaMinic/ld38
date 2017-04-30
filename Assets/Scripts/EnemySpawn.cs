becd0423b83af072a38490746967802539e8aedf
Sava Minić <savam@nordeus.com>
2017-04-30 22:35:41 +0200

    added guitar and enemy spawner

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+
+	public GameObject EnemyPrefab;
+	public float spawnTimer;
+
+	private float nextSpawnTime;
+
+	void Awake()
+	{
+		nextSpawnTime = Time.time + spawnTimer;
+	}
+
+	void Update()
+	{
+		if (GameManager.Instance.IsPlaying && Time.time > nextSpawnTime)
+		{
+			nextSpawnTime = Time.time + spawnTimer;
+			var enemy = Instantiate(EnemyPrefab);
+			enemy.transform.parent = transform.parent;
+			enemy.transform.position = transform.position;
+		}
+	}
+}
